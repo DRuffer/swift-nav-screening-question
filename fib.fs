66edab4295c99002cd6eb25a77951dc0ae682837
Dennis Ruffer <daruffer@gmail.com>
2017-06-04 15:48:21 -0700

    Add Forth and Python solutions
    I found floating point issues in both of these examples on Rosetta, but the Python version seems to not exhibit the problem overtly.

---
+++ fib.fs
@@ -0,0 +1,45 @@
+( This is my favorite language, due to the simplicity of this )
+( Prime @ https://rosettacode.org/wiki/AKS_test_for_primes#Forth )
+( Started from https://rosettacode.org/wiki/Fibonacci_sequence#Forth )
+
+: coeffs ( u -- nu ... n0 ) \ coefficients of (x-1)^u
+   1 swap 1+ dup 1 ?do over over i - i */ negate swap loop drop ;
+ 
+: prime? ( u -- f )
+   dup 2 < if drop false exit then
+   dup >r coeffs 1+
+   \ if not prime, this loop consumes at most half the coefficients, otherwise all
+   begin dup 1 <> while
+      r@ mod 0= while
+   repeat then rdrop
+   dup 1 = >r
+   begin 1 = until
+   r> ;
+ 
+: .monom ( u1 u2 -- )
+   dup 0> if [char] + emit then 0 .r ?dup if ." x^" . else space then ;
+: .poly ( u -- )
+   dup >r coeffs 0 r> 1+ 0 ?do
+      tuck swap .monom 1+
+   loop ;
+ 
+: main
+   11 0 ?do i . ." : " i .poly cr loop cr
+   50 1 ?do i prime? if i . then loop
+   cr ;
+
+: fib ( n -- )   0 1 rot 0 ?do
+      over + swap dup if
+         dup 3 mod 0= if
+            ." Buzz " else
+            dup 5 mod 0= if
+               ." Fizz " else
+               dup 15 mod 0= if
+                  ." FizzBuzz " else
+                  dup prime? if
+                     ." BuzzFizz " else
+                     dup .
+      then then then then else dup . then
+   loop drop ;
+
+( However, I see there some bugs in this AKS algorithm )
